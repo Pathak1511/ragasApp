a8ef82bbb813952e3de1ab135b909231612913a8
Hritik Pathak <hritik1511@gmail.com>
2022-12-15 14:11:38 +0530

    Initial Commit

--- express/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const RagaRoutes = require("./routes/ragasRouter");
-const app = express();
-
-app.use(express.json());
-app.use("/", RagaRoutes);
-
-module.exports = app;
